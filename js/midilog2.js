0d5de1d8822715043f06d1a5fecc5664579afe3b
kj7cadd9 <fabercastell679@yahoo.com>
2017-11-13 04:48:11 +0900

    20171113
    midilog2

--- js/midilog2.js
+++ js/midilog2.js
@@ -37,7 +37,7 @@ function handleMIDIMessage2( event ) {
 	}
 	var targetY = $(log).offset().top+$(log).height();
 	$(log).scrollTop(targetY);
-	var targetX = $(log).offset().left;
+	var targetX = $(log).get(0).selectionStart;
 
 }
 
