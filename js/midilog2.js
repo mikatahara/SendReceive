1893b044c1594b69ad3efbc621bdf224437f0434
kj7cadd9 <fabercastell679@yahoo.com>
2017-11-13 04:06:12 +0900

    20171113
    midilog2

---
+++ js/midilog2.js
@@ -0,0 +1,43 @@
+const LineNum=11;
+const ColumnNum=12;
+var strlog=new Array(LineNum);
+var str=null;
+var mlognum=0;
+
+//add for Page 18, MIDI message monitor
+function makeMassage( event ) {
+	var i,k;
+	str=null;
+
+	if( event.data[0] ==0xFE ) return;
+
+	if( event.data.length>1) {
+
+		str = event.data[0].toString(16) + " ";
+		for(i=1,k=0; i<Math.min(event.data.length,3); i++, k++){
+				if(event.data[i]<0x10) str += "0";
+				str += event.data[i].toString(16);
+				str += " ";
+			}
+		}
+}
+
+function handleMIDIMessage2( event ) {
+	var i,j;
+
+	makeMassage( event );
+
+	log.innerText+=str;
+	log.innerText+=" ";
+	mlognum++;
+	if(mlognum>ColumnNum){
+		log.innerText+="\n";
+		mlognum=0;
+	}
+}
+
+function inputDeviceSelect2(e)
+{
+	inputDeviceSelect(e);
+	input.onmidimessage = handleMIDIMessage2;
+}
